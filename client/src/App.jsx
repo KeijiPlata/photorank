ac2daac202b9f970cd84d0735a1f45dff74f95fb
KeijiPlata <lordmiackykeijip@gmail.com>
2023-02-07 17:32:55 +0800

    Adding registration and login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './Header'
+import Login from './pages/Login'
+import Registration from './pages/Registration'
 
 function App() {
 
   return (
     <div>
-     <Header />
+     <Registration />
     </div>
   )
 }
