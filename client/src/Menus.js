d2ccce8fcdf5abdccbdd336e5a2be42c6f44b547
KeijiPlata <lordmiackykeijip@gmail.com>
2023-02-09 03:06:20 +0800

    Edit footer

--- client/src/Menus.js
+++ client/src/Menus.js
@@ -1,19 +1,17 @@
 export const PRODUCTS = [
-    { name: "Drag And Drop", link: "#" },
-    { name: "Visual Studio X", link: "#" },
-    { name: "Easy Content", link: "#" },
+    { name: "Photos", link: "#" },
+    { name: "Videos", link: "#" },
+    
   ];
   export const RESOURCES = [
     { name: "Industries and tools", link: "#" },
     { name: "Use cases", link: "#" },
     { name: "Blog", link: "#" },
-    { name: "Online evenet", link: "#" },
-    { name: "Nostrud exercitation", link: "#" },
+    { name: "Online event", link: "#" },
   ];
   export const COMPANY = [
-    { name: "Diversity & inclusion", link: "#" },
     { name: "About us", link: "#" },
-    { name: "Press", link: "#" },
+    { name: "SSTech", link: "#" },
     { name: "Customer Stories", link: "#" },
     { name: "Online communities", link: "#" },
   ];
@@ -24,10 +22,3 @@ export const PRODUCTS = [
     { name: "Open-source", link: "#" },
   ];
   
-  export const Icons = [
-    { name: "logo-facebook", link: "#" },
-    { name: "logo-twitter", link: "#" },
-    { name: "logo-github", link: "#" },
-    { name: "logo-linkedin", link: "#" },
-    { name: "logo-instagram", link: "#" },
-  ];
