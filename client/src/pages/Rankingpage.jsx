5b98adb07cffd5476c6688be59e1b6e2e5e40af8
KeijiPlata <lordmiackykeijip@gmail.com>
2023-02-14 21:11:29 +0800

    Add name and number of likes

--- client/src/pages/Rankingpage.jsx
+++ client/src/pages/Rankingpage.jsx
@@ -8,14 +8,16 @@ export default function Rankingpage(){
         <>
         {/* Navigation bar */}
         <Header />
-        
+
         <div className="grid grid-rows-3 grid-flow-col justify-center justify-items-center mt-10">
             <div className="row-start-2 row-span-2">
             <FaCrown className="lg:text-6xl md:text-5xl text-3xl mx-auto md:mb-2 text-gray-400" />
-            <h2 className="text-center font-bold md:text-2xl text-lg ">2nd</h2>
+            <h2 className="text-center font-bold md:text-2xl text-lg myfont">2nd</h2>
             <div style={{ 
       backgroundImage: `url("https://images.unsplash.com/photo-1650378915828-e26d52007540?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80")` 
     }} className='lg:w-64 lg:h-64 w-24 h-24 md:w-56 md:h-56 bg-cover bg-center rounded-full border-solid border-4 border-purple-500'></div>
+        <h2 className="text-center mt-1 font-bold md:text-2xl text-md">Keiji</h2>
+        <p className="text-center mt-1 md:text-xl text-sm text-gray-500">999k likes</p>
             </div>
             <div className="row-end-3 row-span-2">
             <FaCrown className="lg:text-6xl md:text-5xl text-3xl mx-auto md:mb-2 text-yellow-500" />
@@ -23,6 +25,8 @@ export default function Rankingpage(){
             <div style={{ 
       backgroundImage: `url("https://images.unsplash.com/photo-1650378915828-e26d52007540?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80")` 
     }} className='lg:w-64 lg:h-64 w-24 h-24 md:w-56 md:h-56 bg-cover bg-center rounded-full border-solid border-4 border-purple-500'></div>
+    <h2 className="text-center mt-1 font-bold md:text-2xl text-md">Keiji</h2>
+    <p className="text-center mt-1 md:text-xl text-sm text-gray-500">999k likes</p>
             </div>
             <div className="row-start-2 row-span-2">
             <FaCrown className="lg:text-6xl md:text-5xl text-3xl mx-auto md:mb-2 browncolor" />
@@ -30,8 +34,15 @@ export default function Rankingpage(){
             <div style={{ 
       backgroundImage: `url("https://images.unsplash.com/photo-1650378915828-e26d52007540?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80")` 
     }} className='lg:w-64 lg:h-64 w-24 h-24 md:w-56 md:h-56 bg-cover bg-center rounded-full border-solid border-4 border-purple-500'></div>
+    <h2 className="text-center mt-1 font-bold md:text-2xl text-md">Keiji</h2>
+    <p className="text-center mt-1 md:text-xl text-sm text-gray-500">999k likes</p>
     </div>
         </div>
+
+        <div className="mt-5">
+        <Footer />
+        </div>
+        
         </>
     )
 }
